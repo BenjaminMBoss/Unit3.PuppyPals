f05b686c8286f0c6fd9d693f55e4f2aea162439f
Benjamin <benjamin.boss@icloud.com>
2023-11-30 07:11:12 -0500

    Added in Features + Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  
+  const featuredPup = puppies.find((puppy)=> puppy.id === featPupId)
+
   return (
    <div>
-    <h1>Hello World!</h1>
+    <h1>Our Puppies:</h1>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      
+      { featPupId && (
+        <div>
+        <h1> Selected Puppy: </h1>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+        </div>
+      )}
    </div>
   )
 }
